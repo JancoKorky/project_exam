5b8701ccc5d2ba9eb784ee0744b074e8687bf76d
Ján Korkoš <jankorkos@gmail.com>
2022-02-02 20:38:47 +0000

    update doc, commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -7,19 +7,25 @@
 // commands please read more here:
 // https://on.cypress.io/custom-commands
 // ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("login", () => {
+  cy.visit("index.php?route=account/login");
+  cy.get('input[name="email"]').type("yitarip108@chinamkm.com");
+  cy.get('input[name="password"]').type("jozko{enter}");
+  cy.url().should("eq", "https://demo.opencart.com/index.php?route=account/account");
+});
+
+// Cypress.Commands.add("login", () => {
+//   cy.request({
+//     method: "POST",
+//     url: "https://www.opencart.com/index.php?route=account/login",
+//     body: {
+//       user: {
+//         email: "yitarip108@chinamkm.com",
+//         password: "jozko",
+//       },
+//     },
+//   }).then((resp) => {
+//       window.localStorage.setItem('jwt',resp.body.user.token)
+//   });
+// });
